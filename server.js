ec339be38a20f4c699d610be2a7ed105d130e18b
AdamLangevin <langer1911@gmail.com>
2019-03-01 10:06:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -20,31 +20,3 @@ exec(prog, (err, stdout, code) => {
   }
   console.log(stdout);
 });
-/**
-  async () => {
-    await sleep(10000);
-    var opts = {
-      username: 'pi',
-      host: '192.168.0.122',
-      port: '22',
-      password: 'fubar',
-      path: '~/home/pi/Desktop/uploads'
-    };
-    var file = 'C:\\Users\\py120\\Desktop\\Dev\\robotServer\\public\\uploads\\distances.txt';
-
-    client.scp(opts, file, (err) => {
-      if(err) {
-        console.log('Error returning distances: ', err);
-        return;
-      }
-      console.log('Transfered response');
-    });
-  };
-});
-
-function sleep(ms){
-  return new Promise(res => {
-    setTimeout(res,ms);
-  });
-};
-**/
